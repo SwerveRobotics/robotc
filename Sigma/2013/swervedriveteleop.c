bedd7a0f31839b5833a4c7299e2f0c1c08e42b9e
SwerveLap1 <SwerveLap1@localhost>
2013-10-29 21:03:38 -0700

    converted arctan values from radians to degrees. broken encoder.

--- Sigma/2013/swervedriveteleop.c
+++ Sigma/2013/swervedriveteleop.c
@@ -1,6 +1,6 @@
 #pragma config(Hubs,  S1, HTMotor,  HTServo,  HTMotor,  none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Motor,  mtr_S1_C1_1,     motorFR,       tmotorTetrix, openLoop, encoder)
+#pragma config(Motor,  mtr_S1_C1_1,     motorFR,       tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C1_2,     motorBR,       tmotorTetrix, openLoop, encoder)
 #pragma config(Motor,  mtr_S1_C3_1,     motorBL,       tmotorTetrix, openLoop, reversed, encoder)
 #pragma config(Motor,  mtr_S1_C3_2,     motorFL,       tmotorTetrix, openLoop, reversed, encoder)
@@ -17,6 +17,7 @@
 float slowMult = 1;
 int deadZone = 10;
 float degToServo = (255.0/192.0);
+int pos;
 
 task main()
 {
@@ -29,11 +30,11 @@ task main()
 	{
 		getJoystickSettings(joystick);
 
-		int pos = atan(joystick.joy1_y2 / joystick.joy1_x2);
-		if(joystick.joy1_x2 == 0)
+		pos = radiansToDegrees(atan(joystick.joy1_y2 / joystick.joy1_x2));
+		/*if(joystick.joy1_x2 == 0)
 		{
 			pos = 0;
-		}
+		}*/
 		float dist = sqrt(joystick.joy1_y2 * joystick.joy1_y2 + joystick.joy1_x2 * joystick.joy1_x1);
 
 		if (joystick.joy1_Buttons == 6)
@@ -45,70 +46,70 @@ task main()
 			slowMult = 0.3;
 		}
 
-		int motorPower = (int)(dist * slowMult * 0.75);
+		int motorPower = (int)(dist * slowMult);
 
-		if (joystick.joy1_y2 > deadZone && joystick.joy1_x2 > deadZone)
-		{
-			motor[motorFL] = motorPower;
-			motor[motorFR] = motorPower;
-			motor[motorBL] = motorPower;
-			motor[motorBR] = motorPower;
-
-			int servoPos = (int)(pos * degToServo);
-
-			servo[servoFL] = servoPos;
-			servo[servoFR] = servoPos;
-			servo[servoBL] = servoPos;
-			servo[servoBR] = servoPos;
-		}
-		else if (joystick.joy1_y2 > deadZone && joystick.joy1_x2 < (-1 * deadZone))
-		{
-			motor[motorFL] = motorPower;
-			motor[motorFR] = motorPower;
-			motor[motorBL] = motorPower;
-			motor[motorBR] = motorPower;
-
-			int servoPos = (int)((pos + 90) * degToServo);
-
-			servo[servoFL] = servoPos;
-			servo[servoFR] = servoPos;
-			servo[servoBL] = servoPos;
-			servo[servoBR] = servoPos;
-		}
-		else if (joystick.joy1_y2 < (-1 * deadZone) && joystick.joy1_x2 < (-1 * deadZone))
-		{
-			motor[motorFL] = motorPower * -1;
-			motor[motorFR] = motorPower * -1;
-			motor[motorBL] = motorPower * -1;
-			motor[motorBR] = motorPower * -1;
-
-			int servoPos = (int)(pos * degToServo);
-
-			servo[servoFL] = servoPos;
-			servo[servoFR] = servoPos;
-			servo[servoBL] = servoPos;
-			servo[servoBR] = servoPos;
-		}
-		else if (joystick.joy1_y2 < (-1 * deadZone) && joystick.joy1_x2 > deadZone)
-		{
-			motor[motorFL] = motorPower * -1;
-			motor[motorFR] = motorPower * -1;
-			motor[motorBL] = motorPower * -1;
-			motor[motorBR] = motorPower * -1;
-
-			int servoPos = (int)((pos + 90) * degToServo);
-
-			servo[servoFL] = servoPos;
-			servo[servoFR] = servoPos;
-			servo[servoBL] = servoPos;
-			servo[servoBR] = servoPos;
-		}
-		else
+		if (dist < deadZone)
 		{
 			motor[motorFL] = 0;
 			motor[motorFR] = 0;
 			motor[motorBL] = 0;
 			motor[motorBR] = 0;
+		}
+		else if (joystick.joy1_y2 > 0 && joystick.joy1_x2 > 0) // quadrant 1
+		{
+			motor[motorFL] = motorPower;
+			motor[motorFR] = motorPower;
+			motor[motorBL] = motorPower;
+			motor[motorBR] = motorPower;
+
+			int servoPos = (int)((pos + 180) * degToServo);
+
+			servo[servoFL] = servoPos;
+			servo[servoFR] = servoPos;
+			servo[servoBL] = servoPos;
+			servo[servoBR] = servoPos;
+		}
+		else if (joystick.joy1_y2 > 0 && joystick.joy1_x2 < 0) // quadrant 2
+		{
+			motor[motorFL] = motorPower;
+			motor[motorFR] = motorPower;
+			motor[motorBL] = motorPower;
+			motor[motorBR] = motorPower;
+
+			int servoPos = (int)((pos) * degToServo);
+
+			servo[servoFL] = servoPos;
+			servo[servoFR] = servoPos;
+			servo[servoBL] = servoPos;
+			servo[servoBR] = servoPos;
+		}
+		else if (joystick.joy1_y2 < 0 && joystick.joy1_x2 < 0) // quadrant 3
+		{
+			motor[motorFL] = motorPower * -1;
+			motor[motorFR] = motorPower * -1;
+			motor[motorBL] = motorPower * -1;
+			motor[motorBR] = motorPower * -1;
+
+			int servoPos = (int)((pos + 180) * degToServo);
+
+			servo[servoFL] = servoPos;
+			servo[servoFR] = servoPos;
+			servo[servoBL] = servoPos;
+			servo[servoBR] = servoPos;
+		}
+		else if (joystick.joy1_y2 < 0 && joystick.joy1_x2 > 0) // quadrant 4
+		{
+			motor[motorFL] = motorPower * -1;
+			motor[motorFR] = motorPower * -1;
+			motor[motorBL] = motorPower * -1;
+			motor[motorBR] = motorPower * -1;
+
+			int servoPos = (int)((pos) * degToServo);
+
+			servo[servoFL] = servoPos;
+			servo[servoFR] = servoPos;
+			servo[servoBL] = servoPos;
+			servo[servoBR] = servoPos;
 		}
 
 	}
