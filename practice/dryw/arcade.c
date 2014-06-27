477e8f0356db914b089739855c98297b8c8b682e
Fromen <fromen8923@users.noreply.github.com>
2014-06-27 03:52:49 -0600

    Added in comments and moved my deadzone declaration to outside of the while loops.

--- practice/dryw/arcade.c
+++ practice/dryw/arcade.c
@@ -5,10 +5,10 @@
 
 task Drive()
 {
+	int deadZone = 15;
+
 	while(true)
 	{
-		int deadZone = 15;
-
 		quad = Quadrant();
 
 		if(joystick.joy1_y1 < deadZone && joystick.joy1_x1 < deadZone)
@@ -16,22 +16,27 @@ task Drive()
 			LeftMotorPower(0);
 			RightMotorPower(0);
 		}
+		//If the joystick is within the deadzone, none of the motors get any power
+		//Otherwise, find the quadrant the joystick is in and do some mathematical shizzle wizzle to make things happen brah!
 
 		else if(quad == 1)
 		{
 			LeftMotorPower(joystick.joy1_y1);
 			RightMotorPower(abs(joystick.joy1_x1 - 100));
 		}
+
 		else if(quad == 2)
 		{
 			LeftMotorPower(joystick.joy1_x1 + 100);
 			RightMotorPower(joystick.joy1_y1);
 		}
+
 		else if(quad == 3)
 		{
 			LeftMotorPower(0 - abs(joystick.joy1_x1 + 100));
 			RightMotorPower(joystick.joy1_y1);
 		}
+
 		else if(quad == 4)
 		{
 			LeftMotorPower(joystick.joy1_y1);
