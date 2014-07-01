b2f4db0e6586a0be4270782d2ce8b165c87b2f70
Darrell Ross <eukota@users.noreply.github.com>
2014-06-30 22:41:46 -0500

    PID control moved to use simple include files. Still experimenting. Works with Conveyor Bot in BlockParty RVW

--- practice/darrell/pid/pid_controllers.c
+++ practice/darrell/pid/pid_controllers.c
@@ -5,15 +5,35 @@
 #include "pid_includes.h"
 #include "pid_motors.c"
 
+bool enableManualControl = false;
 
+bool ManualControlEnabled()
+{
+	return enableManualControl;
+}
 
 task Controller()
 {
 	int deadZone = 15;
-	
+
 	while(true)
 	{
 		getJoystickSettings(joystick);
+
+
+		// Controller Toggle //
+		if(joy1Btn(1) == 1)
+		{
+			enableManualControl = true;
+		}
+		if(joy1Btn(2) == 1)
+		{
+			enableManualControl = false;
+		}
+		if(!ManualControlEnabled())
+		{
+			continue;
+		}
 
 		if(abs(joystick.joy1_y1) > deadZone)
 		{
@@ -23,7 +43,7 @@ task Controller()
 		{
 			RequestLeftMotorSpeed(0);
 		}
-		
+
 		if(abs(joystick.joy1_y2) > deadZone)
 		{
 			RequestRightMotorSpeed(joystick.joy1_y2); // @todo need to convert output to values in (10,100)
@@ -32,7 +52,7 @@ task Controller()
 		{
 			RequestRightMotorSpeed(0);
 		}
-		
+
 		// @todo add some options for modifying the PID constants while running using available keys //
 		// @todo use some buttons increment and some to descrement //
 		// @todo we can display their values on the NXT LCD Screen //
