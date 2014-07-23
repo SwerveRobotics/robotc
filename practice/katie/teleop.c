da4bd8741528f8506e342eabb2d7fa592f242bea
Katie <Katie@localhost>
2014-07-22 19:29:45 -0700

    assinment #1

--- practice/katie/teleop.c
+++ practice/katie/teleop.c
@@ -5,21 +5,24 @@
 //#pragma debuggerWindows("Servos");
 //#pragma debuggerWindows("joystickSimple");
 
-#include "../Library/motors/motors.c"
+#include "motors.c"
 #include "JoystickDriver.c"
 
-int retVal = 50;
+
 
 task main()
 {
-	retVal = RunMotorForOneSecond(100);
 	while(true)
 	{
 		getJoystickSettings(joystick);
-		// coding assignment //
-		// 1. write a function in Library/motors/motors.c which takes an integer input and sets the motor to drive at that speed
-		// 2. use an if-statement within this while loop to check the left analog joystick y value and pass that to the function written for 1.
-		// 3. BONUS - move the joystick running code to its own task and start that task from within task main() - note this is not easy
 
+		if(abs(joystick.joy1_y1) > 15)
+		{
+			DriveMotor(joystick.joy1_y1);
+		}
+		else
+		{
+			DriveMotor(0);
+		}
 	}
 }
