14517a74b25581c0035ccbcdc7fedca6aa3f7bf7
jarrod <jarrod@localhost>
2014-07-31 20:32:29 -0700

    part of tank drive

---
+++ practice/darrell/motor_defines.h
@@ -0,0 +1,19 @@
+#ifndef MOTOR_DEFINES_H
+#define MOTOR_DEFINES_H
+
+const int MAX_MOTOR_SPEED = 100;// maximum speed we should intentionally give the motor
+const int MIN_MOTOR_SPEED = 20; // minimum speed at which motor will move
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
