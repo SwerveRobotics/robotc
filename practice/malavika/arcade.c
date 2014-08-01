d273a96eb91118e2ffd73c681e9dec1b3ab3da6b
Fromen <fromen8923@users.noreply.github.com>
2014-07-31 20:47:52 -0600

    Automated merge with bundle:c:\users\natalie\appdata\local\temp\thg.e8wbpe\https__hg.codeplex.com_swerverobotics_jqg14c.hg

--- practice/malavika/arcade.c
+++ practice/malavika/arcade.c
@@ -7,7 +7,16 @@ task drive()
 	{
 		getJoystickSettings(joystick);
 
-
+		if((abs(joystick.joy1_x1) < deadZone) && (abs(joystick.joy1_y1) < deadZone))
+		{
+			SetSpeed(left, 0);
+			SetSpeed(right, 0);
+		}
+		else
+		{
+			SetSpeed(left, joystick.joy1_y1 + joystick.joy1_x1);
+			SetSpeed(right, joystick.joy1_y1 - joystick.joy1_x1);
+		}
 	}
 }
 
