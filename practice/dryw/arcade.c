7940504e928439558889f2560191879d6c3cdfbc
Fromen <fromen8923@users.noreply.github.com>
2014-07-31 19:44:22 -0600

    I found the correct way to do arcade drive, so I implemented that and deleted my quadrant detector, as it was useless.

--- practice/dryw/arcade.c
+++ practice/dryw/arcade.c
@@ -1,46 +1,22 @@
 #ifndef DRIVE_C
 #define DRIVE_C
 
-#include "joystickQuadrant.c"
-
 task Drive()
 {
 	int deadZone = 15;
 
 	while(true)
 	{
-		quad = Quadrant();
-
-		if(joystick.joy1_y1 < deadZone && joystick.joy1_x1 < deadZone)
+		if(abs(joystick.joy1_y1) < deadZone && abs(joystick.joy1_x1) < deadZone)
 		{
 			LeftMotorPower(0);
 			RightMotorPower(0);
 		}
-		//If the joystick is within the deadzone, none of the motors get any power
-		//Otherwise, find the quadrant the joystick is in and do some mathematical shizzle wizzle to make things happen brah!
 
-		else if(quad == 1)
+		else
 		{
-			LeftMotorPower(joystick.joy1_y1);
-			RightMotorPower(abs(joystick.joy1_x1 - 100));
-		}
-
-		else if(quad == 2)
-		{
-			LeftMotorPower(joystick.joy1_x1 + 100);
-			RightMotorPower(joystick.joy1_y1);
-		}
-
-		else if(quad == 3)
-		{
-			LeftMotorPower(0 - abs(joystick.joy1_x1 + 100));
-			RightMotorPower(joystick.joy1_y1);
-		}
-
-		else if(quad == 4)
-		{
-			LeftMotorPower(joystick.joy1_y1);
-			RightMotorPower(0 - abs(joystick.joy1_x1 - 100));
+		LeftMotorPower(joystick.joy1_y1 + joystick.joy1_x1);
+		RightMotorPower(joystick.joy1_y1 - joystick.joy1_x1)
 		}
 	}
 }
