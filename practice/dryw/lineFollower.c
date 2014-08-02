3dd3dac905577ecd57adb474a4fd275309343236
Fromen <Fromen@localhost>
2014-08-01 20:20:50 -0600

    I implemented the struct assignment, but the compiler doesn't like what I am doing, even though I'm writing it almost exactly like the website.

--- practice/dryw/lineFollower.c
+++ practice/dryw/lineFollower.c
@@ -1,14 +1,20 @@
 #ifndef DRIVE_C
 #define DRIVE_C
 
+enum {black = 0, white = 1, grey = 2, blue = 3, red = 4, unknown = 5};
+int floorColorEnum;
+
+struct colorRange {
+	int min;
+	int max;
+};
+struct colorRange colorBlue;
 task Drive()
 {
 	while(true)
 	{
-		int floorColorEnum;
-		enum {black = 0, white = 1, grey = 2, blue = 3, red = 4, unknown = 5};
-
-		floorColorEnum = blue;
+		colorBlue.min = 5;
+		floorColorEnum = 2;
 	}
 }
 #endif
