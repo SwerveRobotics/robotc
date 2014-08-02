f3fd752aa2ae1411852ce869c099c421d954cbb6
Fromen <Fromen@localhost>
2014-08-01 18:30:19 -0600

    I created and implemented an enum for the line follower task.

---
+++ practice/dryw/lineFollower.c
@@ -0,0 +1,14 @@
+#ifndef DRIVE_C
+#define DRIVE_C
+
+task Drive()
+{
+	while(true)
+	{
+		int floorColorEnum;
+		enum {black = 0, white = 1, grey = 2, blue = 3, red = 4, unknown = 5};
+
+		floorColorEnum = blue;
+	}
+}
+#endif
