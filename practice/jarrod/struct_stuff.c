64d105c9a0a1ed65c950397b26802547d158c333
jarrod <jarrod@localhost>
2014-08-05 20:39:41 -0700

    Structs and enums *NOT COMPLETE*

---
+++ practice/jarrod/struct_stuff.c
@@ -0,0 +1,27 @@
+#ifndef STRUCT_STUFF
+#define STRUCT_STUFF
+
+typedef enum
+{
+	GRAY_COLOR,
+	BLACK_COLOR,
+	WHITE_COLOR,
+	BLUE_COLOR,
+	RED_COLOR
+
+}Colors;
+
+typedef struct
+{
+	int min;
+	int max;
+}ColorRange;
+
+ int colorStructArray[5];
+
+
+
+
+
+
+#endif
