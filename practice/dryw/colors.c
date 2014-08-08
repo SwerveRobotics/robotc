9bc5f5b99542f197b3b79b2356ca2e9ddcf57aa4
Fromen <Fromen@localhost>
2014-08-08 11:20:55 -0600

    I adjusted my color detection and other minor tweaks.

--- practice/dryw/colors.c
+++ practice/dryw/colors.c
@@ -1,9 +1,39 @@
 #ifndef COLORS_C
 #define COLORS_C
 
-#include "enums.c"
-#include "structs.c"
-#include "initColorVals.c"
+typedef enum
+{
+	BLACK,
+	WHITE,
+	GREY,
+	BLUE,
+	RED
+}
+floorColorEnum;
+
+struct
+{
+	int min;
+	int max;
+}
+colorRange;
+
+colorRange colorArray[6];
+
+void InitColorVals()
+{
+	colorArray[BLACK].min = 0;
+	colorArray[BLACK].max = 1;
+	colorArray[WHITE].min = 2;
+	colorArray[WHITE].max = 3;
+	colorArray[GREY].min = 4;
+	colorArray[GREY].max = 5;
+	colorArray[BLUE].min = 6;
+	colorArray[BLUE].max = 7;
+	colorArray[RED].min = 8;
+	colorArray[RED].max = 9;
+}
+
 #include "colorDetect.c"
 
 #endif
