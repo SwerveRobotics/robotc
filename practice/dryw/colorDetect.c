25a1c413beb50f2d04ad2773da273c30f0363cb3
malavika <malavika@localhost>
2014-08-26 19:31:20 -0700

    my constants weren't in caps. now they are.

---
+++ practice/dryw/colorDetect.c
@@ -0,0 +1,19 @@
+#ifndef COLORDETECT_C
+#define COLORDETECT_C
+
+//If the sensor detects a color, it returns true and displays the color
+//Otherwise it returns false and displays "Unknown"
+bool OnColor(floorColorEnum floorColor, int SensorVal)
+{
+	if(SensorVal > colorArray[floorColor].min && SensorVal > colorArray[floorColor].max)
+	{
+		nxtDisplayTextLine(0, "%d", floorColor);
+		return true;
+	}
+	else
+	{
+		nxtDisplayTextLine(0, "Unknown");
+		return false;
+	}
+}
+#endif
