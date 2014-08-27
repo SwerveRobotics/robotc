b9a0b35c88047fb1190420f130a152c1caf0dc65
malavika <malavika@localhost>
2014-08-26 20:51:39 -0700

    I started working on color detection.

---
+++ practice/dryw/arcade.c
@@ -0,0 +1,23 @@
+#ifndef DRIVE_C
+#define DRIVE_C
+
+task Drive()
+{
+	while(true)
+	{
+		//If the joystick is out of the deazone, some math is done, then that value is given to the motor function
+		//Otherwise it's 0
+		if(abs(joystick.joy1_y1) > deadZone || abs(joystick.joy1_x1) > deadZone)
+		{
+		LeftMotorPower(joystick.joy1_y1 + joystick.joy1_x1);
+		RightMotorPower(joystick.joy1_y1 - joystick.joy1_x1);
+		}
+
+		else
+		{
+			LeftMotorPower(0);
+			RightMotorPower(0);
+		}
+	}
+}
+#endif
