64512a1ee958c63d39c62e14d785dfe1e3c2cee3
Fromen <fromen8923@users.noreply.github.com>
2014-10-09 20:53:05 -0700

    I added in the tank drive task that utilizes the tank_4m.c file to drive the motors regardless of names

---
+++ Library/sensors/sonar.c
@@ -0,0 +1,46 @@
+#ifndef SONAR_C
+#define SONAR_C
+
+//Ratio between centimeters and inches
+float const CENTIMETERS_TO_INCHES = 2.54;
+
+//Displays on the NXT screen how far away the sensor is from an object in inches
+void DisplaySonarOnNXTDisplay()
+{
+	nxtDisplayTextLine(0, "Distance(Inches): %d", SensorValue[sonar] / CENTIMETERS_TO_INCHES);
+}
+
+
+void GoToObject(int move = 0)
+{
+	//The distance we want the robot to be away from an object
+	int targetDistance = 30;
+
+	//The margin of error for the target distance
+	int accuracyMargin = 3;
+
+	//If the function has been told perform the following code
+	if(move == 1)
+	{
+		//If the sensor is farther away from an object than the target distance, the robot moves forward
+		if(SensorValue[sonar] > targetDistance + accuracyMargin)
+		{
+			LeftMotorPower(50);
+			RightMotorPower(50);
+		}
+		//If the sensor is closer to an object than the target distance, the robot moves backward
+		else if(SensorValue[sonar] < targetDistance - accuracyMargin)
+		{
+			LeftMotorPower(-50);
+			RightMotorPower(-50);
+		}
+		//If the robot is within the margin of error from the target distance, the motors stop and the function tells itself to stop
+		else
+		{
+			LeftMotorPower(0);
+			RightMotorPower(0);
+			move = 0;
+		}
+	}
+}
+#endif
