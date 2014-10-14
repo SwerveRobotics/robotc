3c91805ee7b61f9799fead751f539117992e6758
Fromen <Fromen@localhost>
2014-10-13 19:37:52 -0700

    Changed the word "speed" to "power" everywhere I saw it.

--- Library/motors/motors.c
+++ Library/motors/motors.c
@@ -6,9 +6,9 @@
 
 
 // Assuming a linear relationship between these values, we can solve for the slope //
-const float LINEAR_MOTOR_CONTROL_SLOPE = (MAX_MOTOR_SPEED-MIN_MOTOR_SPEED)/(MAX_ANALOG-ANALOG_DEAD_ZONE);
+const float LINEAR_MOTOR_CONTROL_SLOPE = (MAX_MOTOR_POWER-MIN_MOTOR_POWER)/(MAX_ANALOG-ANALOG_DEAD_ZONE);
 // Solve for the Y-intercept (equation was sovled for with a simple system of two equations assuming a linear relationship)
-const float LINEAR_MOTOR_CONTROL_INTERCEPT = (MIN_MOTOR_SPEED-(LINEAR_MOTOR_CONTROL_SLOPE*ANALOG_DEAD_ZONE));
+const float LINEAR_MOTOR_CONTROL_INTERCEPT = (MIN_MOTOR_POWER-(LINEAR_MOTOR_CONTROL_SLOPE*ANALOG_DEAD_ZONE));
 
 // Calculates Output from Input based on a Linear Relationship
 int AttenuateControllerOutput(int output)
@@ -17,13 +17,13 @@ int AttenuateControllerOutput(int output)
   return (LINEAR_MOTOR_CONTROL_SLOPE*output + sgn(output)*LINEAR_MOTOR_CONTROL_INTERCEPT);
 }
 
-// Set Motor Speed
-void SetMotorSpeed(tMotor motorName, int speed)
+// Set Motor Power
+void SetMotorPower(tMotor motorName, int power)
 {
 	if(ASSUME_CONTROLLER_INPUT==true)
-		motor[motorName] = AttenuateControllerOutput(speed);
+		motor[motorName] = AttenuateControllerOutput(power);
 	else
-		motor[motorName] = speed;
+		motor[motorName] = power;
 }
 
 #endif
