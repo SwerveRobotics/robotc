4118cf5f0e4629f37f1aeca1b3dfc27e6b60f332
Fromen <fromen8923@users.noreply.github.com>
2014-10-30 22:41:16 -0700

    Added on to the auto_ramp_kick code and also added 2 Turn(Direction)Time functions

---
+++ Library/autonomous/auto_drive.c
@@ -0,0 +1,71 @@
+#ifndef AUTO_DRIVE_FUNCTIONS_C
+#define AUTO_DRIVE_FUNCTIONS_C
+
+int WHEEL_DIAMETER = 4;
+float GEAR_RATIO = 1.0;
+
+tMotor ENCODER_MOTOR;
+
+void RegisterEncoderMotor(tMotor motorName)
+{
+	ENCODER_MOTOR = motorName;
+}
+
+//Takes an input of encoder ticks and converts to inches
+float EncoderDistance(float ticks)
+{
+	float circumference = PI * WHEEL_DIAMETER;
+	float revolutions = (ticks / 1440.0) * GEAR_RATIO;
+	return revolutions * circumference;
+}
+
+//Drives forward at given power until the distance has been reached
+void DriveForwardDistance(int inches, int power)
+{
+	nMotorEncoder[ENCODER_MOTOR] = 0;
+	while(EncoderDistance(nMotorEncoder[ENCODER_MOTOR]) < inches)
+	{
+		DriveForward(power);
+	}
+	DriveForward(0);
+}
+
+void DriveBackwardDistance(int inches, int power)
+{
+	nMotorEncoder[ENCODER_MOTOR] = 0;
+	while(EncoderDistance(nMotorEncoder[ENCODER_MOTOR]) > -inches)
+	{
+		DriveBackward(power);
+	}
+	DriveBackward(0);
+}
+
+//Turns left until the gyro reads a vaule equal to or greater than the degrees
+void TurnLeftDegrees(int degrees, int power)
+{
+	SensorValue[gyro] = 0;
+	while(abs(SensorValue[gyro]) < degrees)
+	{
+		TurnRight(power);
+	}
+	DriveForward(0);
+}
+
+void TurnLeftTime(int time, int power)
+{
+	TurnLeft(power);
+	wait1Msec(time);
+}
+
+//Turns right until the gyro reads a vaule equal to or greater than the degrees
+void TurnRightDegrees(int degrees, int power)
+{
+	TurnLeftDegrees(degrees, -1 * power);
+}
+
+void TurnRightTime(int time, int power)
+{
+	TurnRight(power);
+	TurnLeft(power);
+}
+#endif
---
+++ ftc8923/initialize_robot.h
@@ -0,0 +1,7 @@
+RegisterDriveMotors(mtrFL, mtrBL, mtrFR, mtrBR);
+RegisterEncoderMotor(mtrFR);
+RegisterIRSeeker(IRSensor);
+WHEEL_DIAMETER = 4;
+GEAR_RATIO = 1.5;
+servo[goalGrabber] = 96;
+servo[irRotator] = 255;
