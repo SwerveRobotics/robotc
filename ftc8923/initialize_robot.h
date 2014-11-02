760294cb596ce4981c4fe2b8c679936d38275810
Fromen <Fromen@localhost>
2014-11-01 18:37:44 -0700

    Added in an inlcudes file and also initialization functions for teleop and autonomous.

--- ftc8923/initialize_robot.h
+++
@@ -1,7 +0,0 @@
-RegisterDriveMotors(mtrFL, mtrBL, mtrFR, mtrBR);
-RegisterEncoderMotor(mtrFR);
-RegisterIRSeeker(IRSensor);
-WHEEL_DIAMETER = 4;
-GEAR_RATIO = 1.5;
-servo[goalGrabber] = 100;
-servo[irRotator] = 255;
