8e8c13b334f6a980edf17b2bff435772b50c2c6c
Darrell Ross <darrell.ross@milsoft.com>
2014-11-07 03:24:11 -0600

    Added a generic tank controller drive task file which can be used with any of our tank modes.

---
+++ Library/controllers/tank_controller.c
@@ -0,0 +1,29 @@
+#ifndef TANK_CONTROLLER_C
+#define TANK_CONTROLLER_C
+
+// NOTE: This task relies on the user having correctly defined their
+// tank drive mode option from those shown in drive_modes.h
+
+#include "JoystickDriver.c"
+#include "../drive_modes/drive_modes.h"
+#include "../controllers/controller_defines.h"
+
+task Drive()
+{
+	while(true)
+	{
+		getJoystickSettings(joystick);
+
+		if(abs(joystick.joy1_y1)>ANALOG_DEAD_ZONE)
+			DriveLeftMotors(joystick.joy1_y1);
+		else
+			StopLeftDriveMotors();
+
+		if(abs(joystick.joy1_y2)>ANALOG_DEAD_ZONE)
+			DriveRightMotors(joystick.joy1_y2);
+		else
+			StopRightDriveMotors();
+	}
+}
+
+#endif
