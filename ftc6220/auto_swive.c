d52a6d551d23a05ad4567839d160393ef1577314
Tate R <Tate R@localhost>
2014-11-06 21:00:06 -0800

    Fixed include errors

--- ftc6220/auto_swive.c
+++ ftc6220/auto_swive.c
@@ -3,9 +3,12 @@
 
 #include "../Library/drive_modes/swerve_4m.c"
 
-const float WHEEL_DIAMETER = 10.16;
+const float WHEEL_CIRCUM = WHEEL_RADIUS * 2.0 *PI;
 const int ENCODER_RESOLUTION = 1024;
 const float ENCODER_TO_CM = (ENCODER_RESOLUTION / MAX_MOTOR_GEAR_RATIO) / (WHEEL_DIAMETER * PI);
+
+// MOVE SWERVE CONSTANTS HERE //
+
 
 void SimpleDriveDirection(int direction, int power, int distance)
 {
