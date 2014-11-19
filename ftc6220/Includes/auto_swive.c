ba44e53d32f2e2687126ba47a715fedacc62a17a
thecombatwombat <thecombatwombat@localhost>
2014-11-18 18:52:58 -0800

    Added an expirimental autonomous and modified auto_swive to include more functions

--- ftc6220/Includes/auto_swive.c
+++ ftc6220/Includes/auto_swive.c
@@ -1,26 +1,60 @@
 #ifndef AUTO_SWIVE_C
 #define AUTO_SWIVE_C
 
-#include "../Library/drive_modes/swerve_4m.c"
+#include "../../Library/drive_modes/swerve_4m.c"
 
-const float WHEEL_CIRCUM = WHEEL_RADIUS * 2.0 *PI;
-const int ENCODER_RESOLUTION = 1024;
-const float ENCODER_TO_CM = (ENCODER_RESOLUTION / MAX_MOTOR_GEAR_RATIO) / (WHEEL_DIAMETER * PI);
+
+int MAX_MOTOR_GEAR_RATIO = 1;   //just added that for testing purposes
+int WHEEL_DIAMETER = 1; //ditto
+const float WHEEL_CIRCUM = WHEEL_RADIUS * 2.0 *PI;			//calculates wheel circumfrence so that CM can be inputed instead of encoder ticks
+const int ENCODER_RESOLUTION = 1024;									//shows number of encoder ticks per rotation. I think this is wrong, and actually should be 1440
+const float ENCODER_TO_CM = (ENCODER_RESOLUTION / MAX_MOTOR_GEAR_RATIO) / (WHEEL_DIAMETER * PI);   		//changes encoder ticks to centimeters so centimeters can be inputed instead. not using this yet. I hope to eventually.
+
 
 // MOVE SWERVE CONSTANTS HERE //
 
+void writeToServos(int value)				//allows all servos to be written to while only typing the value once.
+{
+	servo[BACK_LEFT_SERVO] = value;
+	servo[BACK_RIGHT_SERVO] = value;
+	servo[FRONT_LEFT_SERVO] = value;
+	servo[FRONT_RIGHT_SERVO] = value;
+}
+
+void degreesToMsPulse(int degrees)		//changes degree position of servo to how many MS a pulse needs to be given for
+{
+	int	MsPulse;
+	MsPulse = degrees; //times something to make it work that I haven't figured out yet.
+}
+
+void setServos(int direction)			//sets servos to the correct degree setting using the degreesToMsPulse function. I need to add encoders to ensure accuracy.
+{
+	int MsPulse;
+	degreesToMsPulse(direction);		//calculates MS of pulse based on degrees
+writeToServos(1);									//starts pulse
+	wait1Msec(MsPulse);							//waits calculated number of MS.
+writeToServos(0);									//ends pulse
+}
+
+void setMotors(int power)						//allows all motors to have the same value written to them while only typing the value once
+{
+	motor[BACK_LEFT_MOTOR] = power;
+	motor[BACK_RIGHT_MOTOR] = power;
+	motor[FRONT_LEFT_MOTOR] = power;
+	motor[FRONT_RIGHT_MOTOR] = power;
+}
 
 void SimpleDriveDirection(int direction, int power, int distance)
 {
-	//set motor encoders to zero
-	int distanceDriven = 0;
-	SetServos(direction);
-	while(distanceDriven < distance)
+	nmotorencoder(FRONT_LEFT_MOTOR) = 0; //set motor encoder to zero
+	int distanceDriven = 0;								//set distance driven to 0
+	setServos(direction);									//turn servos to the correct direction
+	while(distanceDriven < distance)			//while distance driven is less than target distance, set motors to the set power and update distance driven
 	{
-		SetMotors(power);
-		distanceDriven = ENCODER_TO_CM * //encoder value
+		setMotors(power);
+		distanceDriven = ENCODER_TO_CM; // times encoder value
 	}
-	SetMotors(0);
+	setMotors(0);													//stop motors
 }
 
 #endif
