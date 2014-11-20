5497556f989daf1d80765815c3a9186dec10b1bf
Darrell Ross <darrell.ross@milsoft.com>
2014-11-19 23:18:32 -0600

    Virtual Meeting Work: Modified ir seeker sensor file to be able to handle up to 4 ir seekers.

--- Library/sensors/ir_seeker.c
+++ Library/sensors/ir_seeker.c
@@ -1,16 +1,38 @@
 #ifndef IR_SEKKER_C
 #define IR_SEEKER_C
 
-tSensors IR_SEEKER;
-
-void RegisterIRSeeker(tSensors IRSeekerName)
+typedef enum
 {
-	IR_SEEKER = IRSeekerName;
+	SeekerOne,
+	SeekerTwo,
+	SeekerThree,
+	SeekerFour
+}SeekerEnum;
+const int SEEKER_COUNT = 4;
+tSensors IR_SEEKER[SEEKER_COUNT];
+
+void RegisterIRSeeker(SeekerEnum seekerNum = 0, tSensors IRSeekerName)
+{
+	switch(seekerNum)
+	{
+		case SeekerOne:
+			IR_SEEKER[SeekerOne] = IRSeekerName;
+			break;
+		case SeekerTwo:
+			IR_SEEKER[SeekerTwo] = IRSeekerName;
+			break;
+		case SeekerThree:
+			IR_SEEKER[SeekerThree] = IRSeekerName;
+			break;
+		case SeekerFour:
+			IR_SEEKER[SeekerFour] = IRSeekerName;
+			break;
+	}
 }
 
-int ReadIRSensor()
+int ReadIRSensor(SeekerEnum seekerNum = 0)
 {
-	return SensorValue[IR_SEEKER];
+	return SensorValue[IR_SEEKER[seekerNum]];
 }
 
 #endif
