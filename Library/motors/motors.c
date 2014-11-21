c1ef868c5552845321d723b89835e2e25e6b9374
Tate R <Tate R@localhost>
2014-11-20 18:46:25 -0800

    Fixed swive bugs and changed instances of meters to cm

--- Library/motors/motors.c
+++ Library/motors/motors.c
@@ -4,6 +4,13 @@
 #include "motor_defines.h"
 #include "../controllers/controller_defines.h"
 
+float MOTOR_GEAR_RATIO;
+float SERVO_GEAR_RATIO;
+
+const float MOTOR_POWER_PER_CMPS = 1.207671; //theoretical motor power nessesary to move at 1 m/s with 4in wheels
+const float MAX_MOTOR_SPEED_CMPS = 81.93;//theoretical max based on 154rpm motor and 4in diameter wheel // gear ratio?
+
+const float SERVO_TICK_PER_DEGREE = 1.4118;//number of servo ticks nessesary to rotate 1 degree
 
 // Assuming a linear relationship between these values, we can solve for the slope //
 const float LINEAR_MOTOR_CONTROL_SLOPE = (MAX_MOTOR_POWER-MIN_MOTOR_POWER)/(MAX_ANALOG-ANALOG_DEAD_ZONE);
@@ -24,6 +31,22 @@ void SetMotorPower(tMotor motorName, int power)
 		motor[motorName] = AttenuateControllerOutput(power);
 	else
 		motor[motorName] = power;
+}
+
+//Set Motor Power from a CM/S value
+void mpsToMotor(tMotor motorName, float cmps)
+{
+	motor[motorName] = 	MOTOR_POWER_PER_CMPS * MOTOR_GEAR_RATIO * cmps;
+}
+
+void degToCRServo(TServoIndex servoName, int angle)
+{
+	//sig code here
+}
+
+void degToServo(TServoIndex servoName, int angle)
+{
+	servo[servoname] = SERVO_GEAR_RATIO * SERVO_TICK_PER_DEGREE * angle;
 }
 
 #endif
