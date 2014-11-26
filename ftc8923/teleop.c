c8ef60e579c48d348862bfc7581b405589b805f4
Jack the awesome <Jack the awesome@localhost>
2014-11-25 21:05:50 -0800

    Added the motor to pick up balls.

--- ftc8923/teleop.c
+++ ftc8923/teleop.c
@@ -1,11 +1,13 @@
-#pragma config(Hubs,  S1, HTMotor,  HTServo,  none,     none)
+#pragma config(Hubs,  S1, HTMotor,  HTServo,  HTMotor,  none)
 #pragma config(Hubs,  S4, HTMotor,  none,     none,     none)
 #pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S2,     gyroSensor,           sensorI2CHiTechnicGyro)
+#pragma config(Sensor, S2,     gyroSensor,     sensorI2CHiTechnicGyro)
 #pragma config(Sensor, S3,     IRSensor,       sensorHiTechnicIRSeeker600)
 #pragma config(Sensor, S4,     ,               sensorI2CMuxController)
 #pragma config(Motor,  mtr_S1_C1_1,     mtrFL,         tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S1_C1_2,     mtrBL,         tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_1,     collector,     tmotorTetrix, openLoop)
+#pragma config(Motor,  mtr_S1_C3_2,     motorG,        tmotorTetrix, openLoop)
 #pragma config(Motor,  mtr_S4_C1_1,     mtrBR,         tmotorTetrix, openLoop, reversed)
 #pragma config(Motor,  mtr_S4_C1_2,     mtrFR,         tmotorTetrix, openLoop, reversed, encoder)
 #pragma config(Servo,  srvo_S1_C2_1,    goalGrabber,          tServoStandard)
@@ -41,6 +43,18 @@ task main()
 		if(joy1Btn(7) == 1)
 		{
 			ReleaseGoal();
+		}
+		if(joy2Btn(8) == 1)
+		{
+			CollectBalls();
+		}
+		else if(joy2Btn(7) == 1)
+		{
+			ReleaseBalls();
+		}
+		else
+		{
+			StopCollector();
 		}
 	}
 }
