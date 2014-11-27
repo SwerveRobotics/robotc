74af6b73c3ea8e39903a3831ae5936d609fd672f
Fromen <fromen8923@users.noreply.github.com>
2014-11-26 19:05:55 -0800

    Made functions for lifter when it's finally attached. Still incomplete, as there should be 2 motors declared, but we don't know where they will go.

--- ftc8923/include_files/functions.c
+++ ftc8923/include_files/functions.c
@@ -1,6 +1,7 @@
 #ifndef FTC8923_FUNCTIONS
 #define FTC8923_FUNCTIONS
 
+//Goal grabber functions
 void GrabGoal()
 {
 	servo[goalGrabber] = 170;
@@ -10,16 +11,122 @@ void ReleaseGoal()
 {
 	servo[goalGrabber] = 100;
 }
+
+//Collector functions
 void CollectBalls()
 {
 	motor[collector] = -100;
 }
+
 void ReleaseBalls()
 {
 	motor[collector] = 100;
 }
+
 void StopCollector()
 {
 	motor[collector] = 0;
 }
+
+//Lift functions
+
+//Commented out as the motors haven't yet been declared. We can't declare them yet as we don't know how they will be wired
+/*
+void LowerLift()
+{
+	motor[lifter] = -100;
+}
+
+void RaiseLift()
+{
+	motor[lifter] = 100;
+}
+
+void StopLift()
+{
+	motor[lifter] = 0;
+}
+
+//Lift position functions
+void LiftDownPos()
+{
+	if(above)
+	{
+		while(above)
+		{
+			LowerLift();
+		}
+	}
+	StopLift();
+}
+
+void LiftSmallPos()
+{
+	if(above)
+	{
+		while(above)
+		{
+			LowerLift();
+		}
+	}
+	if(below)
+	{
+		while(below)
+		{
+			RaiseLift();
+		}
+	}
+	StopLift();
+}
+
+void LiftMediumPos()
+{
+	if(above)
+	{
+		while(above)
+		{
+			LowerLift();
+		}
+	}
+	if(below)
+	{
+		while(below)
+		{
+			RaiseLift();
+		}
+	}
+	StopLift();
+}
+
+void LiftHighPos()
+{
+	if(above)
+	{
+		while(above)
+		{
+			LowerLift();
+		}
+	}
+	if(below)
+	{
+		while(below)
+		{
+			RaiseLift();
+		}
+	}
+	StopLift();
+}
+
+void LiftCenterPos()
+{
+	if(below)
+	{
+		while(below)
+		{
+			RaiseLift();
+		}
+	}
+	StopLift();
+}
+*/
 #endif
