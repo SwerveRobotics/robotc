cc16f3e209b5d91db51079d3a17398f46e18bc3c
Darrell Ross <darrell.ross@milsoft.com>
2014-12-03 23:30:47 -0600

    Added some gyro test code to test drive-straight functionality.

--- ftc_drive_a_bot/auto_gyro_test.c
+++ ftc_drive_a_bot/auto_gyro_test.c
@@ -14,6 +14,7 @@
 #include "../library/drive_modes/drive_modes.h"
 #include "../library/sensors/gyro.c"
 #include "../library/drive/gyro_drive.c"
+#include "../library/controllers/tank_controller.c"
 
 const int TURN_TEST_POWER = 30;
 
@@ -28,18 +29,25 @@ task main()
 	RegisterDriveMotors(mot1, mot2);
 	RegisterGyroSensor(gyroSensor);
 
+	StartTask(DriveTank);
+
 	while(true)
 	{
 		if(joy1Btn(1)==1)
 		{
+			StopTask(DriveTank);
 			TurnLeftDegrees(90,TURN_TEST_POWER);
+			StartTask(DriveTank);
 		}
 		if(joy1Btn(2)==1)
 		{
+			StopTask(DriveTank);
 			TurnRightDegrees(90,TURN_TEST_POWER);
+			StartTask(DriveTank);
 		}
 		if(joy1Btn(3)==1)
 		{
+			StopTask(DriveTank);
 			TurnLeftDegrees(90,TURN_TEST_POWER);
 			wait1Msec(1000);
 			TurnRightDegrees(180,TURN_TEST_POWER);
@@ -48,8 +56,33 @@ task main()
 			wait1Msec(1000);
 			TurnRightDegrees(90,TURN_TEST_POWER);
 			wait1Msec(1000);
+			StartTask(DriveTank);
 		}
-		StopAllDriveMotors();
+		if(joy1Btn(4)==1) // drive straight using gyro as long as button is held down
+		{
+			StopTask(DriveTank);
+			// Button Down //
+			StopAllDriveMotors();
+			wait1Msec(250); //The wait is here to ensure the robot comes to a stop before calibrating the gyro
+			ResetEncoderValue();
+			startGyro();
+			wait1Msec(200);
+			// @todo use gyroValid() functionality somehow
+			resetGyro();
+			ClearTimer(T4);
+			while(joy1Btn(4)==1)
+			{
+				int drivePower = 50;
+				InitSpecialGyroDrive(DriveActionForwardForever, drivePower);
+				if(!SpecialGyroDrive(DriveActionForwardForever, drivePower))
+					break;
+				wait1Msec(200); // so we don't attempt to adjust power 60 times a second thus reducing one motor power to zero
+			}
+			// Button Up //
+			StopAllDriveMotors();
+			stopGyro();
+			StartTask(DriveTank);
+		}
 	}
 
 }
