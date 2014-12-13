fc75b766dd78099a51f9654927b51931d3bba9c5
Jack the awesome <Jack the awesome@localhost>
2014-12-13 13:47:55 -0800

    Make it so the desplay goes forward stops and goes backwards.

---
+++ ftc8923/include_files/center_objectives.c
@@ -0,0 +1,40 @@
+#ifndef CENTER_OBJECTIVES_C
+#define CENTER_OBJECTIVES_C
+
+int CENTER_GOAL_POSITION = 0;
+
+void DectectFloorIR()
+{
+	wait1Msec(500);
+	if(ReadIRSensor(1) == 4)
+	{
+		CENTER_GOAL_POSITION = 2;
+	}
+	else if(ReadIRSensor(1) == 3)
+	{
+		CENTER_GOAL_POSITION = 1;
+	}
+	else if(ReadIRSensor(1) == 5)
+	{
+		CENTER_GOAL_POSITION = 3;
+	}
+}
+
+void RampKickPos1()
+{
+	TurnLeftDegrees(50, 50);
+	DriveForwardDistanceGyro(60, 50);
+}
+
+void RampKickPos2()
+{
+	DriveForwardDistanceGyro(10, 50);
+	TurnLeftDegrees(75, 50);
+	DriveForwardDistanceGyro(35, 50);
+}
+
+void RampKickPos3()
+{
+
+}
+#endif
