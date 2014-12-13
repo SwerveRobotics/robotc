6b4fdb00a54decb502b490fac836b2d429ef79c6
Jack the awesome <Jack the awesome@localhost>
2014-12-13 13:47:55 -0800

    Make it so the desplay goes forward stops and goes backwards.

--- ftc8923/experimental_files/8923_display.c
+++ ftc8923/experimental_files/8923_display.c
@@ -3,37 +3,39 @@
 
 task main()
 {
-	const int defaultSpeed = 50;
-	int motorSpeed = defaultSpeed;
-	const int speedDifference = 10;
+	int motorSpeed;
+	const int defaultSpeed = 15;
+ 	const int backwardsSpeed = -15;
+ 	const int stopped = 0;
 	while(true)
 	{
 		//Right arrow to speed up
   	if(nNxtButtonPressed == 1)
   	{
+  		motorSpeed = defaultSpeed;
   		while(nNxtButtonPressed == 1)
   		{
 
   		}
-   		motorSpeed += speedDifference;
   	}
   	//Left arrow to slow down
   	if(nNxtButtonPressed == 2)
 		{
+			motorSpeed = backwardsSpeed;
 			while(nNxtButtonPressed == 2)
 			{
 
 			}
-			motorSpeed -= speedDifference;
+
 		}
 		//Center button to return to normal speed
 		if(nNxtButtonPressed == 3)
 		{
+			motorSpeed = stopped;
 			while(nNxtButtonPressed == 3)
 			{
 
 			}
-			motorSpeed = defaultSpeed;
 		}
 		motor[Lift] = motorSpeed;
 	}
