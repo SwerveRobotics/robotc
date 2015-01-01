bcedaaa0e184775db23c2d27169aac37c5d77893
Fromen <fromen8923@users.noreply.github.com>
2015-01-01 00:13:24 -0800

    Made a task which will figure out where walls are.

--- practice/dryw/map_writer_bot/main.c
+++ practice/dryw/map_writer_bot/main.c
@@ -14,6 +14,6 @@ task main()
 {
 	InitBot();
 
-	MapRoom();
+	//MapRoom();
 	//ReturnHome();
 }
--- practice/dryw/map_writer_bot/map_designer.c
+++ practice/dryw/map_writer_bot/map_designer.c
@@ -1,13 +1,23 @@
 #ifndef MAP_DESIGNER
 #define MAP_DESIGNER
 
-void MapRoom()
+//void MapRoom()
+//{
+//	while(true)
+//	{
+//		DriveBackwardDistanceGyro(3, DEFAULT_POWER);
+//		TurnLeftDegrees(90, DEFAULT_POWER);
+//		DriveUntilObjectHit(DEFAULT_POWER);
+//	}
+//}
+
+task MapRoom()
 {
 	while(true)
 	{
-		DriveBackwardDistanceGyro(3, DEFAULT_SPEED);
-		TurnLeftDegrees(90, DEFAULT_SPEED);
-		DriveUntilObjectHit(DEFAULT_SPEED);
+		//Sensor mapping
+
+		//Touch mapping
 	}
 }
 
