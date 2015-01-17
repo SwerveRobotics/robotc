cb12a3ea10e9e0e0aa174ff6edee518db6884db8
Darrell Ross <eukota@users.noreply.github.com>
2015-01-17 04:35:43 -0600

    FTC 8923 - Code Review. Reviewed all files that seemed relevent for this year except for those in Experimental and library.

--- ftc8923/include_files/includes.h
+++ ftc8923/include_files/includes.h
@@ -1,3 +1,10 @@
+
+/* Code Review by Darrell
+ - *Great work using an include of includes.*
+ - *Very clean code.*
+ - File lacks the #ifndef wrapper per style guide.
+*/
+
 #include "../../Library/sensors/ir_seeker.c"
 #include "../../Library/drive/gyro_drive.c"
 #include "functions.c"
