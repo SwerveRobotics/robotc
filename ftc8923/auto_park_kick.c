957f0dc4aff7919110eb472bb9d20cc98840cff9
Jack the awesome <Jack the awesome@localhost>
2015-01-23 19:51:31 -0800

    Got auto_park_kick working well.

--- ftc8923/auto_park_kick.c
+++ ftc8923/auto_park_kick.c
@@ -39,8 +39,8 @@ task main()
 
 	//Get positioned to detect beacon
 	DriveBackwardDistanceGyro(10, 50);
-	TurnLeftDegrees(55, 30);
-	DriveBackwardDistanceGyro(27, 20);
+	TurnLeftDegrees(70, 30);
+	DriveBackwardDistanceGyro(21, 20);
 
 	DectectFloorIR();
 
