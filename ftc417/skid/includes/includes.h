1306c645f1065d2fec1a73bd8510267d176ee61b
Darrell Ross <eukota@users.noreply.github.com>
2015-01-26 13:59:23 -0600

    Review of 417/skid's files.

--- ftc417/skid/includes/includes.h
+++ ftc417/skid/includes/includes.h
@@ -1,6 +1,16 @@
 #ifndef INCLUDES_H
 #define INCLUDES_H
 
+/*Darrell Review
+	file should probably reflect the type of includes since you will have two different types
+	  autonomous
+	  teleop
+
+	autonomous has no need to include the joystick driver.
+	If you want to use the same includes file, then you should move the JoystickDriver.c include to the individual files that need it
+*/
+
+
 #include "JoystickDriver.c"
 #include "../../../library/drive_modes/parallel_pairs.c"
 #include "../../../library/drive_modes/drive_modes.h"
