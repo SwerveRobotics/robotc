0fc9eb3f2609b5205de383d93e2bf17b7b7602a7
Jack the awesome <Jack the awesome@localhost>
2015-02-02 21:00:17 -0800

    Added a zeroing function to our lift.

--- ftc8923/include_files/teleop_includes.c
+++ ftc8923/include_files/teleop_includes.c
@@ -1,11 +1,14 @@
 #ifndef INCLUDES_H
 #define INCLUDES_H
 
-#include "../Library/controllers/tank_controller.c"
-#include "include_files/functions.c"
-#include "include_files/init_teleop.c"
 #include "../drivers/hitechnic-sensormux.h"
 #include "../drivers/lego-touch.h"
 #include "../drivers/hitechnic-irseeker-v2.h"
+// The sensor define has to be here for the next few files
+const tMUXSensor touchSensor = msensor_S3_1;
+const tMUXSensor irSensor = msensor_S3_2;
+#include "../Library/controllers/tank_controller.c"
+#include "include_files/functions.c"
+#include "include_files/init_teleop.c"
 
 #endif
