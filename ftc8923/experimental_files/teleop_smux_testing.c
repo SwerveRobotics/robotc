35c11b1f120311daa4871541ab126834849337d7
Fromen <Fromen@localhost>
2015-02-02 19:16:36 -0800

    Modified main files to use the SMUX

--- ftc8923/experimental_files/teleop_smux_testing.c
+++ ftc8923/experimental_files/teleop_smux_testing.c
@@ -28,7 +28,7 @@
 
 #include "../../drivers/hitechnic-sensormux.h"
 #include "../../drivers/lego-touch.h"
-#include "../drivers/hitechnic-irseeker-v2.h"
+#include "../../drivers/hitechnic-irseeker-v2.h"
 
 const tMUXSensor touchSensor = msensor_S3_1;
 const tMUXSensor irSensor = msensor_S3_2;
---
+++ ftc8923/include_files/teleop_includes.c
@@ -0,0 +1,11 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+#include "../Library/controllers/tank_controller.c"
+#include "include_files/functions.c"
+#include "include_files/init_teleop.c"
+#include "../drivers/hitechnic-sensormux.h"
+#include "../drivers/lego-touch.h"
+#include "../drivers/hitechnic-irseeker-v2.h"
+
+#endif
